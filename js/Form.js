b6ea663d15ba48043f955cda6c51ee6e758ea13d
mayookhm <79088427+mayookhm@users.noreply.github.com>
2021-09-11 16:13:43 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -11,7 +11,7 @@ class Form{
         this.greeting.hide();
         this.button.hide();
         this.input.hide();
-        this.title.hide();
+        
     }
     display() {
         this.title.html("FRUIT CATCHER");
